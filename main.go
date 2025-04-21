ec63bbd95e611c89c953768ef0c0527d0dc0dc56
Kevin <kevin.kds80@gmail.com>
2025-04-21 10:27:00 -0300

    move logic to main

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"imagery/concurrent"
 	"imagery/files"
 )
 
 func main() {
 	fmt.Println("Imagery")
-	files.Open("oi")
+	m, err := files.Open("C:/Users/Usuario/Documents/golang/imagery/examples/fons-heijnsbroek.jpg")
+	if err != nil {
+		return
+	}
+	img := concurrent.Run(m)
+	files.Save(img)
 }
