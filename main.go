2b20b3f7adf1956085834e820b0c56e15c4b9d0c
Kevin <kevin.kds80@gmail.com>
2025-04-21 14:30:33 -0300

    move logic to cli

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"imagery/concurrent"
-	"imagery/files"
-	"imagery/transformations"
+	"imagery/cli"
 )
 
 func main() {
-	fmt.Println("Imagery")
-	m, err := files.Open("C:/Users/Usuario/Documents/golang/imagery/examples/fons-heijnsbroek.jpg")
-	if err != nil {
-		return
-	}
-	img := concurrent.Run(m, transformations.TransformationOptions{MaskSize: 3, Multiplier: 1}, transformations.Negative)
-	files.Save(img)
+	cli.Entrypoint()
 }
